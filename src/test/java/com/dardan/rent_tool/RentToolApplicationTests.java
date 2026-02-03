d7c699e10514739e73ed4b663503e1dd045cb460
Alejor0205 <dlejo.r02p@gmail.com>
2026-02-03 12:31:02 -0500

    feat: :tada: Incio del proyecto

---
+++ src/test/java/com/dardan/rent_tool/RentToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dardan.rent_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
