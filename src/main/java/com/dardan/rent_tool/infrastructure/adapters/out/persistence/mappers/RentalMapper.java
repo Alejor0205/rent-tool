14d5be0603ffaa7eac88787b63fa5f461a0b99e2
Alejor0205 <dlejo.r02p@gmail.com>
2026-02-04 16:43:07 -0500

    fix: :bug: correcion de error

--- src/main/java/com/dardan/rent_tool/infrastructure/adapters/out/persistence/mappers/RentalMapper.java
+++ src/main/java/com/dardan/rent_tool/infrastructure/adapters/out/persistence/mappers/RentalMapper.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import com.dardan.rent_tool.domain.model.entity.Rental;
 import com.dardan.rent_tool.infrastructure.adapters.out.persistence.entity.RentalEntity;
 import com.dardan.rent_tool.infrastructure.adapters.out.persistence.entity.UserEntity;
+import java.util.UUID;
 
 public class RentalMapper {
     public Rental toDomain(RentalEntity entity) {
