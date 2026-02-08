9519784903bdd99a1f20493ffe4a7ec02ca8b23e
Alejor0205 <dlejo.r02p@gmail.com>
2026-02-08 14:02:32 -0500

    feat: :sparkles: dependecias

--- src/main/java/com/dardan/rent_tool/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/dardan/rent_tool/infrastructure/config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/tools/**").permitAll()
                 .requestMatchers("/api/admin/**").hasRole("ADMIN")
